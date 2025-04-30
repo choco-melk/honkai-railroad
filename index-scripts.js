72001a038bd554a263c4fc21f013620f7b76cad5
Your_Name <154525691+choco-melk@users.noreply.github.com>
2025-04-30 17:36:18 +0800

    Added Login Session

---
+++ index-scripts.js
@@ -0,0 +1,14 @@
+/* | FIELDS */
+/* || Sections */
+const registerForm = document.getElementById('register-form');
+const startContainer = document.getElementById('start-container');
+
+/* || Links */
+const startButton = document.getElementById('start-game-button');
+
+
+/* | Functions */
+/* || EVENTLISTENERS */
+startButton.addEventListener({
+    
+});
