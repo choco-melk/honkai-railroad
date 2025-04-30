f9ec65f0c38c764f2cd6022ee428730158b26ad8
Your_Name <154525691+choco-melk@users.noreply.github.com>
2025-04-30 18:23:50 +0800

    Updated Login Session

--- index-scripts.js
+++ index-scripts.js
@@ -1,14 +1,25 @@
 /* | FIELDS */
 /* || Sections */
-const registerForm = document.getElementById('register-form');
+const loginForm = document.getElementById('login-form');
 const startContainer = document.getElementById('start-container');
-
 /* || Links */
 const startButton = document.getElementById('start-game-button');
+const logOutButton = document.getElementById('logout-container');
 
 
 /* | Functions */
 /* || EVENTLISTENERS */
-startButton.addEventListener({
-    
+startButton.addEventListener('click', e => {
+    e.preventDefault();     
+    loginForm.style.display = 'none';
+    startContainer.style.display = 'block';
+    logOutButton.style.display = 'block';
 });
+logOutButton.addEventListener('click', e => {
+    e.preventDefault();     
+    loginForm.style.display = 'block';
+    logOutButton.style.display = 'none';
+    startContainer.style.display = 'none';
+});
+
+
