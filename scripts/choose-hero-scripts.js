7969a5321af34bb7996523188c1be8dfd4539096
Your_Name <154525691+choco-melk@users.noreply.github.com>
2025-05-05 17:09:59 +0800

    Added Starter Trailblazer Menu

---
+++ scripts/choose-hero-scripts.js
@@ -0,0 +1,31 @@
+/* | FIELDS */
+/* || HTML Elements */
+const title = document.getElementById('title-content');
+const button = document.getElementById('confirm-button');
+const stelle = document.getElementById('character-stelle');
+const caelus = document.getElementById('character-caelus');
+/* || States */
+let starterChar = '';
+
+/* | Functions */
+/* || EVENTLISTENERS */
+stelle.addEventListener('click', e => {
+    e.preventDefault();
+    title.innerText = "Selecting Trailblazer Stelle";
+    stelle.style.transform = 'scale(1.1)';
+    stelle.style.filter = 'brightness(100%)';
+    caelus.style.transform = 'scale(1.0)';
+    caelus.style.filter = 'brightness(75%)';
+    button.style.display = 'block'; 
+});
+caelus.addEventListener('click', e => {
+    e.preventDefault();    
+    title.innerText = "Selecting Trailblazer Caelus";
+    caelus.style.transform = 'scale(1.1)';
+    caelus.style.filter = 'brightness(100%)';
+    stelle.style.transform = 'scale(1.0)';
+    stelle.style.filter = 'brightness(75%)';
+    button.style.display = 'block'; 
+});
+
+
--- scripts/index-scripts.js
+++ scripts/index-scripts.js
@@ -1,25 +1,46 @@
 /* | FIELDS */
-/* || Sections */
+/* || HTML Elements */
+const body = document.getElementById('index');  
+const bgVideo = document.getElementById('bg-video');
+/* ||| Sections */
 const loginForm = document.getElementById('login-form');
 const startContainer = document.getElementById('start-container');
+
 /* || Links */
 const startButton = document.getElementById('start-game-button');
 const logOutButton = document.getElementById('logout-container');
 
+/* || States */
+let login_valid = false;
 
 /* | Functions */
 /* || EVENTLISTENERS */
+body.addEventListener('click', e => {
+    if (login_valid) {
+        e.preventDefault();
+        window.location.replace('choose-hero.html');
+    }
+});
 startButton.addEventListener('click', e => {
     e.preventDefault();     
     loginForm.style.display = 'none';
-    startContainer.style.display = 'block';
+    startContainer.style.display = 'flex';
     logOutButton.style.display = 'block';
+    bgVideo.removeAttribute('class');
+    login_valid = true;
+    e.stopImmediatePropagation();
 });
 logOutButton.addEventListener('click', e => {
     e.preventDefault();     
     loginForm.style.display = 'block';
     logOutButton.style.display = 'none';
     startContainer.style.display = 'none';
+    bgVideo.setAttribute('class', 'darken');
+    login_valid = false;
 });
+
+
+
+
 
 
